9c14f4c0c4c24090de78a4bde3141062b6c3ac55
Artur <arturbrnd1@gmail.com>
2026-05-14 15:45:08 -0300

    feat: create character filter types

---
+++ frontend/src/types/filters.ts
@@ -0,0 +1,10 @@
+export type CharacterFilterGender = "female" | "male" | "genderless";
+export type CharacterFilterStatus = "alive" | "dead" | "unknown";
+
+export interface CharacterFilters {
+  name?: string;
+  status?: CharacterFilterStatus;
+  species?: string;
+  type?: string;
+  gender?: CharacterFilterGender;
+}

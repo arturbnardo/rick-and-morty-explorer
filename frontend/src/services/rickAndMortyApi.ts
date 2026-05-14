7c8ddd4d772c46d761034ad6f1541fafee7b9688
Artur <arturbrnd1@gmail.com>
2026-05-14 19:24:24 -0300

    feat: implement character api service

---
+++ frontend/src/services/rickAndMortyApi.ts
@@ -0,0 +1,37 @@
+import type { Character, PaginatedResponse } from "../types/character";
+import type { CharacterFilters } from "../types/filters";
+const BASE_URL = "https://rickandmortyapi.com/api";
+
+export const getCharacters = async (
+  filter?: CharacterFilters,
+): Promise<PaginatedResponse<Character>> => {
+  const params = new URLSearchParams();
+
+  if (filter) {
+    if (filter.name) params.append("name", filter.name);
+    if (filter.status) params.append("status", filter.status);
+    if (filter.species) params.append("species", filter.species);
+    if (filter.type) params.append("type", filter.type);
+    if (filter.gender) params.append("gender", filter.gender);
+  }
+
+  const response = await fetch(`${BASE_URL}/character?${params}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Request failed");
+  }
+};
+
+export const getCharacterById = async (id: number): Promise<Character> => {
+  const response = await fetch(`${BASE_URL}/character/${id}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Request failed");
+  }
+};

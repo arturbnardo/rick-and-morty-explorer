08e0b0971d833d7f0d9cbabcf11d061a9df8f8b6
Artur <arturbrnd1@gmail.com>
2026-06-03 15:44:29 -0300

    feat: add use character details hook

---
+++ frontend/src/hooks/useCharacterDetails.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { getCharacterById } from "../services/rickAndMortyApi";
+import type { Character } from "../types/character";
+import { isAxiosError } from "axios";
+import toast from "react-hot-toast";
+
+interface useCharacterDetailsProps {
+  id: number;
+}
+
+export const useCharacterDetails = ({ id }: useCharacterDetailsProps) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<Character | null>(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchCharactersId = async () => {
+      try {
+        setLoading(true);
+        const charactersId = await getCharacterById(id);
+        setData(charactersId);
+      } catch (err) {
+        if (isAxiosError(err) && err.status === 404) {
+          toast.error("Failed to find characters. Please try again.");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCharactersId();
+  }, [id]);
+
+  return { loading, data, error };
+};

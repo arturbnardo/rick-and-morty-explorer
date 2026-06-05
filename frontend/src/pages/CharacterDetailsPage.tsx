d4b03a5d09b986925f3cd2d5046983935e728561
Artur <arturbrnd1@gmail.com>
2026-06-04 23:49:04 -0300

    feat: implement character details page

--- frontend/src/pages/CharacterDetailsPage.tsx
+++ frontend/src/pages/CharacterDetailsPage.tsx
@@ -1,5 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useCharacterDetails } from "../hooks/useCharacterDetails";
 function CharacterDetailsPage() {
-  return <div>This is the CharacterDetailsPage</div>;
+  const { id } = useParams();
+  const characterId = Number(id);
+
+  const { loading, data, error } = useCharacterDetails({
+    id: characterId,
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  if (data === null) return <p>Character not found</p>;
+
+  return (
+    <div>
+      <div>This is the CharacterDetailsPage</div>
+    </div>
+  );
 }
 
 export default CharacterDetailsPage;

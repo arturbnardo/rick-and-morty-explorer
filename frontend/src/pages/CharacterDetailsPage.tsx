bebba2d732b0af44bd49a31a1e0f8615f1bc2022
Artur <arturbrnd1@gmail.com>
2026-06-17 13:31:12 -0300

    feat: implement character details page

--- frontend/src/pages/CharacterDetailsPage.tsx
+++ frontend/src/pages/CharacterDetailsPage.tsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { useCharacterDetails } from "../hooks/useCharacterDetails";
 import StatusChip from "../components/StatusChip";
 function CharacterDetailsPage() {
@@ -16,32 +15,24 @@ function CharacterDetailsPage() {
   if (data === null) return <p>Character not found</p>;
 
   return (
-    <div className="flex gap-8">
-      <img src={data.image} alt={data.name} />
-      <div className="flex flex-col">
-        <h1>{data.name}</h1>
-        <StatusChip status={data.status} species={data.species} />
-        <p>Gender: {data.gender}</p>
-        <p>Origin: {data.origin.name}</p>
-        <p>Last known location: {data.location.name}</p>
-        <p>Episodes: {data.episode.length}</p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex overflow-hidden bg-gray-800 rounded-2xl border border-cyan-800">
+        <img src={data.image} alt={data.name} className="w-80 object-cover" />
 
-        <Link
-          to="/"
-          className="
-          mt-auto
-          px-6
-          py-3
-          rounded-xl
-          bg-cyan-700
-          font-bold
-          text-white
-          transition-colors duration-300
-          hover:bg-cyan-900
-          hover:cursor-pointer"
-        >
-          HomePage
-        </Link>
+        <div className="flex flex-col justify-center text-white font-bold p-6 gap-6">
+          <h1 className="text-4xl font-bold pl-4">{data.name}</h1>
+
+          <div className="flex flex-col gap-2">
+            <StatusChip status={data.status} species={data.species} />
+
+            <div className="pl-4 flex flex-col gap-2">
+              <p>Gender: {data.gender}</p>
+              <p>Origin: {data.origin.name}</p>
+              <p>Last known location: {data.location.name}</p>
+              <p>Episodes: {data.episode.length}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

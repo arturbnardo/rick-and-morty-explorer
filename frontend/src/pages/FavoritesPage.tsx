75a5495aaed813d1af53f5eb22eefad54da81559
Artur <arturbrnd1@gmail.com>
2026-06-26 21:15:37 -0300

    style: improve responsive layout

--- frontend/src/pages/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage.tsx
@@ -7,11 +7,13 @@ function FavoritesPage() {
 
   if (favorites.length === 0)
     return (
-      <div className="text-center my-20 flex flex-col gap-4">
-        <p className="text-3xl text-white font-semibold">
+      <div className="min-h-screen flex items-center justify-center flex-col gap-4">
+        <p className="md:text-3xl text-2xl text-white font-semibold">
           Your favorites list is empty.
         </p>
-        <p className="text-gray-400">Start exploring!</p>
+        <p className="md:text-3xl text-2xl text-white font-semibold">
+          Start exploring!
+        </p>
         <Link
           to="/"
           className="text-cyan-400 hover:text-cyan-600 text-2xl font-bold transition-colors duration-200"
@@ -23,11 +25,11 @@ function FavoritesPage() {
 
   return (
     <div>
-      <h1 className="text-center text-cyan-400 text-3xl my-4">
+      <h1 className="text-center text-cyan-400 text-2xl md:text-3xl my-8 ">
         My Favorites (
         <span className="text-white font-bold">{favorites.length}</span>)
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 max-w-360 mx-auto px-8 my-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 max-w-360 mx-auto px-8 my-4">
         {favorites.map((character) => (
           <CharacterCard key={character.id} character={character} />
         ))}

a1de76d91c4380d0b27701e27de51a4be1049c01
Artur <arturbrnd1@gmail.com>
2026-06-26 21:55:01 -0300

    fix: add page to character filters type

--- frontend/src/types/filters.ts
+++ frontend/src/types/filters.ts
@@ -2,6 +2,7 @@ export type CharacterFilterGender = "female" | "male" | "genderless";
 export type CharacterFilterStatus = "alive" | "dead" | "unknown";
 
 export interface CharacterFilters {
+  page?: number;
   name?: string;
   status?: CharacterFilterStatus;
   species?: string;

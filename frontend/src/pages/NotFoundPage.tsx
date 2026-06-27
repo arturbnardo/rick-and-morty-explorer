bbcbcb8c38a918d87c0891bd1c545e14a4f55f14
Artur <arturbrnd1@gmail.com>
2026-06-26 21:14:20 -0300

    feat: create not found page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 function NotFoundPage() {
-  return <div>This is the NotFoundPage</div>;
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center flex-col gap-4">
+        <p className="text-5xl text-white font-bold">404</p>
+        <p className="md:text-3xl text-lg text-white font-semibold">
+          The page you're looking for doesn't exist.
+        </p>
+        <Link
+          to="/"
+          className="text-cyan-400 hover:text-cyan-600 text-2xl font-bold transition-colors duration-200"
+        >
+          Home
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
